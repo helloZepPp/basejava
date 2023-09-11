0825004eb073535502035345e46302310bbf46bc
helloZepPp <48869684+helloZepPp@users.noreply.github.com>
2023-09-11 19:23:28 +0300

    Update ArrayStorage.java (new global field: size)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,102 +3,75 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    int size;
 
     void clear() {
 
         for (int i = 0; i < storage.length; i++) {
 
-            if (storage[i] != null) {
-    	        storage[i] = null;
-    		}
+            if (size != 0 && storage[i] != null) {
+                storage[i] = null;
+                size--;
+            }
 
-    	}
+        }
 
     }
-
-
 
     void save(Resume r) {
 
         for (int i = 0; i < storage.length; i++) {
 
             if (storage[i] == null) {
-    	        storage[i] = r;
-    	        break;
-    		}
+                storage[i] = r;
+                size++;
+                break;
+            }
 
-    	}
+        }
 
     }
-
-
 
     Resume get(String uuid) {
 
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].toString() == uuid) {return storage[i];}
+            if (storage[i] != null && storage[i].toString() == uuid) {
+                return storage[i];
+            }
         }
 
-    	return null;
+        return null;
 
     }
-
-
 
     void delete(String uuid) {
 
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].toString() == uuid) {storage[i] = null; break;}
+            if (storage[i] != null && storage[i].toString() == uuid) {
+                storage[i] = null;
+                size--;
+                break;
+            }
         }
 
     }
-
-
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        Resume[] vRes = new Resume[size()];
 
-    	/* сначала нашел такое решение, но потом прочитал в ТЗ, что так нельзя
-        List<Resume> list = new ArrayList<Resume>();
-        for(Resume r : storage) {
-           if(r != null && storage.length > 0) {list.add(r);}
-        }
-        Resume[] v_res = list.toArray(new Resume[list.size()]);
-        */
-
-        int v_size  = 0; // кол-во непустых элементов массива storage
-        int v_count = 0; // счетчик для нового массива
-
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {v_size++;}
+        for (int i = 0; i < size(); i++) {
+            vRes[i] = storage[i];
         }
 
-        Resume[] v_res = new Resume[v_size];
-
-
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-            	v_res[v_count] = storage[i]; v_count++;
-            }
-        }
-
-    	return v_res;
+        return vRes;
 
     }
 
-
-
     int size() {
-        int v_res = 0;
-
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {v_res++;}
-        }
-
-        return v_res;
+        return size;
     }
 
 }
